02fb14fe24ad08a910c8f734c5e64050e24b36ba
Nafisha Aslam <nafishaaslam2@gmail.com>
2026-02-16 15:58:50 +0530

    first commit

--- FRONTEND/src/components/ItemManagement.jsx
+++ FRONTEND/src/components/ItemManagement.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import "../styles/ItemManagement.css"
+import "../styles/itemManagement.css"
 
 const API_URL = 'http://localhost:8000/api'
 
