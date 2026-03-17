b7a409e530523dcbfcf53d86c942a0a8ef7a9f56
Nafisha Aslam <146452545+Nafisha08@users.noreply.github.com>
2026-03-17 21:13:49 +0530

    Update API proxy URL in vite.config.js

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy: {
-      '/api':'http://localhost:8000',
+      '/api':'https://aqma-queue-management-1.onrender.com',
     },
     // Configure CORS for WebSocket connections
     cors: true,
